af51cda5f359f350ca6e60ad802cd04c49c8d02b
Rajdeep <160480035+rajdeep-314@users.noreply.github.com>
2024-06-09 20:39:36 +0530

    Update koch.py
    Added colour modes

--- koch.py
+++ koch.py
@@ -4,6 +4,7 @@
 
 import turtle
 import math
+from colorsys import hsv_to_rgb
 
 # To keep track of whether lengths are being diluted to zero
 _dilution_warning = False
@@ -14,12 +15,23 @@ turtle.color('white')
 turtle.hideturtle()
 turtle.speed(10)
 
+turtle.width(2)
 
 # Draws a "segment" of the snowflake
 # length : total horizontal length of the final structure
-def koch(stage, length):
+# colour -> 0: no colouring; 1: grayscale colouring; 2: weird HSV-based colouring
+def koch(stage, length, colour=0):
     # Base case
     if stage == 0:
+        if colour == 1:     # Grayscale colouring
+            k_factor = 0.1
+            colour_ = k_factor + (1-k_factor)*abs(turtle.heading() - 180)/180
+            turtle.color(*[colour_]*3)
+        elif colour == 2:   # HSV-based colouring
+            k_factor = 0.1
+            hsv_colour = k_factor + (1-k_factor)*abs(turtle.heading() - 180)/180
+            rgb_colour = hsv_to_rgb(hsv_colour, 1, 1)
+            turtle.color(*rgb_colour)
         turtle.fd(length)
     else:
         prev_stage = stage - 1
@@ -32,25 +44,25 @@ def koch(stage, length):
             _dilution_warning = True
 
         # Implementing recursion
-        koch(prev_stage, prev_length)
+        koch(prev_stage, prev_length, colour=colour)
         turtle.lt(60)
-        koch(prev_stage, prev_length)
+        koch(prev_stage, prev_length, colour=colour)
         turtle.rt(120)
-        koch(prev_stage, prev_length)
+        koch(prev_stage, prev_length, colour=colour)
         turtle.lt(60)
-        koch(prev_stage, prev_length)
+        koch(prev_stage, prev_length, colour=colour)
 
 
 # Helper function to pass some parameters before drawing the segment
 # align=True -> The generated structure will be symmetrical about the turtle's current position, along the turtle's current direction
-def draw_segment(stage, length, align=False, fast=False):
+def draw_segment(stage, length, align=False, fast=False, colour=0):
     if fast:
         turtle.tracer(0)
     # Initializing position for alignment
     if align:
         turtle.teleport(x=turtle.pos()[0] - length/2)
 
-    koch(stage, length)
+    koch(stage, length, colour=colour)
     
     # Returning to original position post-alignment
     if align:
@@ -61,7 +73,7 @@ def draw_segment(stage, length, align=False, fast=False):
 
 # Draws the koch flake with the given parameters
 # align=True -> the current postion becomes the flake's centroid
-def draw_flake(stage, length, align=False, fast=False):
+def draw_flake(stage, length, align=False, fast=False, colour=0):
     # Initializing position for alignment
     if align:
         current_pos = turtle.pos()
@@ -69,7 +81,7 @@ def draw_flake(stage, length, align=False, fast=False):
 
     # Generating the flake
     for i in range(3):
-        draw_segment(stage, length, align=False, fast=fast)
+        draw_segment(stage, length, align=False, fast=fast, colour=colour)
         turtle.rt(120)
 
     # Returning to original position post-alignment
