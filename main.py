0f3263c398136322bbdea0a85a4fb9f973e73f90
Rajdeep <160480035+rajdeep-314@users.noreply.github.com>
2024-06-09 20:49:23 +0530

    Update main.py
    Added colours to the demo

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import time
 
 # Displays Koch snowflakes of stages 1 to 5, aligned at the window's centre (without animation)
 for stage in range(6):
-    draw_flake(stage, 729, align=True, fast=True)
+    draw_flake(stage, 729, align=True, fast=True, colour=2)
 
 time.sleep(2)        # Waits for 2 seconds
 turtle.clear()       # Clears the screen
 
 # Displays Koch snowflakes of stages 4 to 1, aligned at the window's centre (with animation)
-for stage in reversed(range(4)):
-    draw_flake(stage, 729, align=True, fast=False)
+# Also cycles between the various colour modes
+for stage in range(4):
+    draw_flake(stage, 729, align=True, fast=False, colour=stage%3)
+    time.sleep(0.8)
+    turtle.clear()
 
 turtle.mainloop()
